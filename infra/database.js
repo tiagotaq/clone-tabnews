90e12e55c33e979d218391c1a3c013f41e80ff9f
Tiago Carvalho Ferreira de Andrade <101513606+tiagotaq@users.noreply.github.com>
2026-03-22 20:57:02 +0000

    add a more robust ssl configuration method

--- infra/database.js
+++ infra/database.js
@@ -7,14 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
-  console.log("Credenciais do Postgres:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
   });
   try {
     await client.connect();
@@ -26,6 +19,17 @@ async function query(queryObject) {
   } finally {
     await client.end();
   }
+}
+
+function getSSLValues() {
+  //Deixei sem o certificado (a variável POSTGRES_CA não existe) porque não criei a conta no DigitalOcean
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
 }
 
 export default {
